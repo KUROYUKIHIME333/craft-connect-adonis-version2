0573fd1d173f34d5c58793125e0cb5bb412f018f
KUROYUKIHIME333 <dhermansum@gmail.com>
2024-11-30 17:01:58 +0100

    auth controller makinig

---
+++ app/controllers/auth_controller.ts
@@ -0,0 +1,11 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user'
+import argon2 from 'argon2'
+import axios from 'axios'
+
+export default class AuthController {
+  async registration({}: HttpContext) {
+    try {
+    } catch (error) {}
+  }
+}
