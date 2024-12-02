907d23725a222171bfaf888c4778f850b346b693
KUROYUKIHIME333 <dhermansum@gmail.com>
2024-12-02 14:59:29 +0100

    user and craftman registration

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,11 +1,102 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import User from '#models/user'
+import Craftman from '#models/craftman'
 import argon2 from 'argon2'
 import axios from 'axios'
+//import { messages } from '@vinejs/vine/defaults'
 
 export default class AuthController {
-  async registration({}: HttpContext) {
+  async userRegistration({ request: req, response: res }: HttpContext) {
     try {
-    } catch (error) {}
+      const datas = req.only([
+        'fullName',
+        'profilePicture',
+        'email',
+        'gender',
+        'birthdate',
+        'password',
+        'phoneNumber',
+        'mobileMoneyNumber',
+        'address',
+      ])
+
+      const userVerified = await User.query().where('email', datas.email).first()
+
+      if (userVerified) {
+        return res.status(201).json({
+          message: 'User already exist',
+        })
+      } else {
+        const user = new User()
+
+        user.fullName = datas.fullName
+        user.profilePicture = datas.profilePicture
+        user.email = datas.email
+        user.gender = datas.gender
+        user.birthdate = datas.birthdate
+        user.password = await argon2.hash(datas.password)
+        user.phoneNumber = datas.phoneNumber
+        user.mobileMoneyNumber = datas.mobileMoneyNumber
+        user.address = datas.address
+
+        user.save()
+
+        return res.status(201).json({
+          message: 'User creation : success',
+        })
+      }
+    } catch (error) {
+      return res.status(201).json({
+        message: 'An error has occured',
+        error: error.message,
+      })
+    }
+  }
+
+  async craftmanRegistration({ request: req, response: res }: HttpContext) {
+    try {
+      const datas = req.only([
+        'userId',
+        'specialities',
+        'fields',
+        'certifications',
+        'activityArea',
+        'professionalAddress',
+        'identityDoc',
+        'identityDocNumber',
+        'expirationDate',
+        'identityDocPics',
+      ])
+
+      const craftmanVerified = await Craftman.query().where('userId', datas.userId).first()
+
+      if (craftmanVerified) {
+        return res.status(201).json({
+          message: 'Craftman already exist',
+        })
+      } else {
+        const craftman = new Craftman()
+
+        craftman.userId = datas.userId
+        craftman.specialities = datas.specialities
+        craftman.fields = datas.fields
+        craftman.certifications = datas.certifications
+        craftman.activityArea = datas.activityArea
+        craftman.professionalAddress = datas.professionalAddress
+        craftman.identityDoc = datas.identityDoc
+        craftman.identityDocNumber = datas.identityDocNumber
+        craftman.expirationDate = datas.expirationDate
+        craftman.identityDocPics = datas.identityDocPics
+
+        return res.status(201).json({
+          message: 'Craftman creation : success',
+        })
+      }
+    } catch (error) {
+      return res.status(201).json({
+        message: 'An error has occured',
+        error: error.message,
+      })
+    }
   }
 }
